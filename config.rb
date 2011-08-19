a41b345f9c2fb4d7fca0f7b77882b9515327cf11
Benjamin R Doerr <tiggerizzy@gmail.com>
2011-08-19 14:01:10 -0400

    Playing with getting CNAME and url correct.

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/bendoerr.me/"
+http_images_path = "/bendoerr.me/images"
+http_fonts_path = "/bendoerr.me/fonts"
+css_dir = "public/bendoerr.me/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
